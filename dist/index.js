f870db3e0cf9a8df61e6e156fef7bde58fc766da
Francesca Dreith <francesca.dreith@gmail.com>
2020-09-15 15:18:53 -0700

    typescript practice

--- dist/index.js
+++ dist/index.js
@@ -9,26 +9,85 @@ var app = express_1.default();
 var port = 3000; // default port to listen
 // define a route handler for the default home page
 app.get("/", function (req, res) {
-    res.send(appendSnacks);
+    res.send("I love snacks!");
 });
 // start the server
 app.listen(port, function () {
     console.log("server running at http://localhost:" + port);
 });
-////////////////////
-var snacks = [
+// TYPESCRIPT TYPES:
+// STRINGS
+var potatoChips = "Potato Chips";
+var dip = "Dip";
+var foodILove = "\n  I love " + potatoChips + " and " + dip + "!\n";
+// BOOLEANS
+var hungry = true;
+// NUMBERS
+// include floating point, hexidecimal, and decimal literals
+// As of ECMAScript 2015 octal literals and biancy
+var num = 8 + 0xf00d + 10 + 484;
+// ARRAYS
+var favoriteSnacks = [
     "popcorn",
-    "hamburgers",
-    "gummy bears",
-    "potato chips",
     "tacos",
+    "gummy worms",
+    "cheese puffs",
 ];
-// const snackDiv = document.createElement("div");
-var appendSnacks = function () {
-    // snacks.forEach((snack) => {
-    //   snackDiv.insertAdjacentHTML("beforeend", `-- ${snack}`);
-    // });
-    // snackDiv.innerHTML;
-    snacks.forEach(function (snack) { return "" + snack; });
+// let snacks: Array<string> = favoriteSnacks;
+// TUPLES
+var meal = ["chips and salsa", { tacos: 3 }];
+// OBJECTS
+// Anything that is non-primative. Not a number, string, boolean, symbol, null, or undefined.
+var obj = new Map();
+var hambuger = {
+    bun: 2,
+    patty: 1,
+    lettuce: 1,
+    tomato: 1,
+    onion: 2,
+    sauce: "the perfect amount",
 };
-// document.addEventListener("DOMContentLoaded", appendSnacks);
+//ENUMS
+// enum Snack {
+//   Chips,
+//   Popcorn,
+//   Gummies,
+//   Apple,
+// }
+// let snack: Snack = Snack.Popcorn;
+// ANY
+var mySnack;
+mySnack = "popcorn";
+mySnack = 10;
+mySnack = true;
+mySnack = { tasty: true };
+// VOID
+var emptyStomachWarning = console.log("Change hungry status to true");
+// NULL AND UNDEFINED
+var missedLunch = null;
+var exactTimeForSnack = undefined;
+// as-syntax
+var someFood = "hamburger";
+var foodLength = someFood.length;
+// angle-bracket syntax
+var someotherFood = "hamburger";
+var otherFoodLength = someFood.length;
+// let snacks: string[] = "potato chips";
+var snacks = ["potato chips"];
+var chips = { name: "potato chips", quantity: 10, tasty: true };
+function eatSnack(snack) {
+    console.log("Chomp chomp, " + snack.name + " " + (snack.quantity > 1 ? "are" : "is") + "\n    " + (snack.tasty ? "tasty!" : "not so great.."));
+}
+eatSnack(chips);
+// => Chomp chomp, potato chips are tasty!
+function eat(food, quantity) {
+    for (var i = 0; i < quantity; i++) {
+        console.log("chomp, " + food);
+    }
+}
+eat(chips, 5);
+// => chomp! potato chips
+// => chomp! potato chips
+// => chomp! potato chips
+// => chomp! potato chips
+// => chomp! potato chips
--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,122 @@ app.get("/", function (req, res) {
 app.listen(port, () => {
   console.log(`server running at http://localhost:${port}`);
 });
+
+// TYPESCRIPT TYPES:
+
+// STRINGS
+const potatoChips: string = "Potato Chips";
+const dip: string = "Dip";
+const foodILove = `
+  I love ${potatoChips} and ${dip}!
+`;
+
+// BOOLEANS
+let hungry: boolean = true;
+
+// NUMBERS
+// include floating point, hexidecimal, and decimal literals
+// As of ECMAScript 2015 octal literals and biancy
+let num: number = 8 + 0xf00d + 0b1010 + 0o744;
+
+// ARRAYS
+let favoriteSnacks: string[] = [
+  "popcorn",
+  "tacos",
+  "gummy worms",
+  "cheese puffs",
+];
+// let snacks: Array<string> = favoriteSnacks;
+
+// TUPLES
+let meal: [string, object] = ["chips and salsa", { tacos: 3 }];
+
+// OBJECTS
+// Anything that is non-primative. Not a number, string, boolean, symbol, null, or undefined.
+const obj: object = new Map();
+const hambuger: object = {
+  bun: 2,
+  patty: 1,
+  lettuce: 1,
+  tomato: 1,
+  onion: 2,
+  sauce: "the perfect amount",
+};
+
+//ENUMS
+// enum Snack {
+//   Chips,
+//   Popcorn,
+//   Gummies,
+//   Apple,
+// }
+
+// let snack: Snack = Snack.Popcorn;
+
+// ANY
+let mySnack: any;
+mySnack = "popcorn";
+mySnack = 10;
+mySnack = true;
+mySnack = { tasty: true };
+
+// VOID
+let emptyStomachWarning: void = console.log("Change hungry status to true");
+
+// NULL AND UNDEFINED
+let missedLunch: null = null;
+let exactTimeForSnack: undefined = undefined;
+
+// as-syntax
+let someFood: unknown = "hamburger";
+let foodLength: number = (someFood as string).length;
+
+// angle-bracket syntax
+let someotherFood: unknown = "hamburger";
+let otherFoodLength: number = (<string>someFood).length;
+
+// let snacks: string[] = "potato chips";
+
+let snacks: string[] = ["potato chips"];
+interface Snack {
+  name: string;
+  quantity: number;
+  tasty: boolean;
+  flavor?: string;
+}
+
+let chips = { name: "potato chips", quantity: 10, tasty: true };
+
+function eatSnack(snack: Snack) {
+  console.log(
+    `Chomp chomp, ${snack.name} ${snack.quantity > 1 ? "are" : "is"}
+    ${snack.tasty ? "tasty!" : "not so great.."}`
+  );
+}
+
+eatSnack(chips);
+// => Chomp chomp, potato chips are tasty!
+
+// Named function with Typescript
+function eat(food: string, quantity: number) {
+  for (let i = 0; i < quantity; i++) {
+    console.log(`CHOMP! mmm, ${food}`);
+  }
+}
+
+//Anonymous function with Typescript
+let eatMore = function (food: string, quantity: number): void {
+  for (let i = 0; i < quantity; i++) {
+    console.log(`CHOMP! mmm, ${food}`);
+  }
+};
+
+eat("potato chips", 5);
+
+const doritos = { name: "doritos" };
+
+let snackBreak = function (food = "hot dog", quantity: number): void {
+  console.log(`Tasty, ${food}!`);
+};
+
+snackBreak("chips", 15);
